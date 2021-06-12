a2f3a2fc1edaf391c54b3a8b34dc99cbcbbf20d1
PANGSORAMDEPO <pangsoramdepo@gmail.com>
2021-06-12 22:25:14 +0700

    Config: Eureka Client and API Gatway For Routing To Micro Service

--- src/main/java/com/pangsoramdepo/microservice/MicroserviceApplication.java
+++ src/main/java/com/pangsoramdepo/microservice/MicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.pangsoramdepo.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroserviceApplication {
 
 	public static void main(String[] args) {
